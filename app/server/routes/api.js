184ff6078da49e4df905bd3d8ab3b82837247f66
Kevin Gill <hkevgill@hotmail.com>
2016-12-21 14:56:08 -0800

    Added connection pool, finished get queries

--- app/server/routes/api.js
+++ app/server/routes/api.js
@@ -14,21 +14,36 @@ module.exports = function(app, express) {
         res.json({ message: 'post works!' });
     });
 
+    // get all courses
+    apiRouter.get('/courses', function(req, res) {
+        databaseService.getCourses(req, res)
+    });
+
+    // get specific course and all students attending it and a list of all students
+    apiRouter.get('/courses/:courseId', function(req, res) {
+        console.log(req.params.courseId);
+        databaseService.getEditCourseInfo(req, res);
+    });
+
+    // insert courses csv to courses table
     apiRouter.get('/insertcourses', function(req, res) {
         csvService.insertCSV('Courses.csv', 'Courses');
         res.json({ message: 'success' });
     });
 
+    // insert students csv to students table
     apiRouter.get('/insertstudents', function(req, res) {
         csvService.insertCSV('Students.csv', 'Students');
         res.json({ message: 'success' });
     });
 
+    // insert registration csv to registration table
     apiRouter.get('/insertregistration', function(req, res) {
         csvService.insertCSV('Registration.csv', 'Registration');
         res.json({ message: 'success' });
     });
 
+    // insert contact information csv to contact information table
     apiRouter.get('/insertcontactinformation', function(req, res) {
         csvService.insertCSV('Contact_Information.csv', 'Contact_Information');
         res.json({ message: 'success' });
--- app/server/services/database-service.js
+++ app/server/services/database-service.js
@@ -1,7 +1,20 @@
 var pg = require('pg');
 
-var connection = 'postgres://dlbbsvuarifngz:b194a723dfe33f623dd721b44bf064bb82ca7a9554947a1f7f943a533f7636cb@ec2-54-163-233-89.compute-1.amazonaws.com:5432/dl6islujmsp1u?ssl=true';
-var pg = new pg.Client(connection);
+var config = {
+    user: 'dlbbsvuarifngz',
+    database: 'dl6islujmsp1u',
+    password: 'b194a723dfe33f623dd721b44bf064bb82ca7a9554947a1f7f943a533f7636cb',
+    host: 'ec2-54-163-233-89.compute-1.amazonaws.com', // Server hosting the postgres database
+    port: 5432,
+    ssl: true,
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+// var connection = 'postgres://dlbbsvuarifngz:b194a723dfe33f623dd721b44bf064bb82ca7a9554947a1f7f943a533f7636cb@ec2-54-163-233-89.compute-1.amazonaws.com:5432/dl6islujmsp1u?ssl=true';
+// var pg = new pg.Client(connection);
+
+var pool = new pg.Pool(config);
 
 module.exports.create = function(req, res) {
     pg.connect();
@@ -35,4 +48,76 @@ module.exports.create = function(req, res) {
     }
 
     createCourses.on('end', students);
+}
+
+module.exports.getCourses = function(req, res) {
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query('SELECT * FROM Courses', function(err, result) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            res.json(result);
+        });
+    });
+}
+
+module.exports.getEditCourseInfo = function(req, res) {
+
+    var data = {};
+
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query('SELECT * FROM Courses WHERE Course_ID = $1::int', [req.params.courseId], function(err, result) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            data.course = result.rows[0];
+
+            // get registered students
+            client.query('SELECT * FROM Students NATURAL JOIN (SELECT R.Student_ID FROM Courses as C NATURAL JOIN Registration as R WHERE C.Course_ID = $1::int) AS derivedTable', [req.params.courseId], function(err, result) {
+
+                //call `done()` to release the client back to the pool
+                done();
+
+                if(err) {
+                    return console.error('error running query', err);
+                }
+
+                data.registeredStudents = result.rows;
+
+                // get list of all students
+                client.query('SELECT * FROM Students', function(err, result) {
+
+                    //call `done()` to release the client back to the pool
+                    done();
+
+                    if(err) {
+                        return console.error('error running query', err);
+                    }
+
+                    data.students = result.rows;
+
+                    res.json(data);
+                });
+            });
+
+        });
+    });
 }
